59de356363f8eef7b8b999c23dec77724105e3b3
Viktoria Nikolaeva <nv020814@gmail.com>
2023-03-23 21:54:25 +0300

    Create class SimpleLinkedListWrapper

---
+++ src/SimpleLinkedListWrapper.java
@@ -0,0 +1,27 @@
+public class SimpleLinkedListWrapper<T> implements SimpleList<T> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
