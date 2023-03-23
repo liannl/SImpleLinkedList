341cbc39781a5d1ad7a64b6a13534b215e7401ac
Viktoria Nikolaeva <nv020814@gmail.com>
2023-03-23 22:00:14 +0300

    Implementation of the class SimpleLinkedListWrapper

--- src/SimpleLinkedListWrapper.java
+++ src/SimpleLinkedListWrapper.java
@@ -1,27 +1,65 @@
 public class SimpleLinkedListWrapper<T> implements SimpleList<T> {
+    private Node<T> head, tail;
+    private int size;
+    public SimpleLinkedListWrapper() {
+        head = tail = null;
+        size =0;
+    }
 
+    private static class Node<T>{
+        T data;
+        Node<T> next;
+        Node(T data){
+            this.data = data;
+            next = null;
+        }
+    }
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
-    @Override
-    public T get(int index) {
-        return null;
-    }
 
     @Override
     public void add(T value) {
-
+        Node<T> newNode = new Node<>(value);
+        if (head == null){
+            head = tail = newNode;
+        } else {
+            Node<T> current = tail;
+            current.next = newNode;
+            tail = current.next;
+        }
+        size++;
     }
 
     @Override
     public void remove(int index) {
-
+        if(index<0 || index>=size){
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0){
+            head = head.next;
+        }else {
+            Node<T> current = head;
+            for (int i = 0;i<index -1 ;i++){
+                current = current.next;
+            }
+            if (index == size-1){
+                tail = current;
+            }
+            current.next = current.next.next;
+        }
+        size--;
     }
 
     @Override
     public void print() {
-
+        Node<T> current = head;
+        while (current != null){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
     }
 }
--- src/SimpleList.java
+++ src/SimpleList.java
@@ -1,6 +1,5 @@
 public interface SimpleList<T> {
     int size();
-    T get(int index);
     void add(T value);
     void remove(int index);
     void print();
